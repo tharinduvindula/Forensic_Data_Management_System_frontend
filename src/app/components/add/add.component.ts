db23196301e07f3a88e532ffc94f12d63b64f2a4
tharinduvindula <tharinduvindula@gmail.com>
2020-05-05 17:46:57 +0530

    modify colombo map

--- src/app/components/add/add.component.ts
+++ src/app/components/add/add.component.ts
@@ -51,43 +51,59 @@ export class AddComponent implements OnInit {
   sp1: {
     name:string;
   }[]=[
-    {name: "Blood"},
-    {name: "Liver"},
-    {name: "Suspected Poison"},
-    {name: "Urine"},
-    {name: "Kidney"},
+    {name: 'Blood'},
+    {name: 'Liver'},
+    {name: 'Suspected Poison'},
+    {name: 'Urine'},
+    {name: 'Kidney'},
   
 ];
  sp2: {
     name:string;
   }[]=[
-    {name: "Tablets/Medicines"},
-    {name: "Bile"},
-    {name: "Lungs"},
-    {name: "Other (Specify)"},
+    {name: 'Tablets/Medicines'},
+    {name: 'Bile'},
+    {name: 'Lungs'},
+    {name: 'Other (Specify)'},
   ];
   sp3: {
     name:string;
   }[]=[
-    {name:"Stomach Contents"},
-    {name: "Vitreous humor"},
-    {name: "Intestinal Contents"},
-    {name: "Brain"},
+    {name:'Stomach Contents'},
+    {name: 'Vitreous humor'},
+    {name: 'Intestinal Contents'},
+    {name: 'Brain'},
   ];
  
 
 // tslint:disable-next-line: member-ordering
   areas: Area[] = [
-    {value: 'area-01', viewValue: 'Agulana'}, {value: 'area-02', viewValue: 'Athurugiriya'}, {value: 'area-03', viewValue: 'Bambalapitiya'},
-    {value: 'area-04', viewValue: 'Boralesgamuwa'}, {value: 'area-05', viewValue: 'Borella'}, {value: 'area-06', viewValue: 'Dehiwala'},
-// tslint:disable-next-line: max-line-length
-    {value: 'area-07', viewValue: 'Homagama'}, {value: 'area-08', viewValue: 'Hospital Police'}, {value: 'area-09', viewValue: 'Jayawardanapura'},
-    {value: 'area-10', viewValue: 'Kahathuduwa'}, {value: 'area-11', viewValue: 'Kirulapone'}, {value: 'area-12', viewValue: 'Kohuwala'},
-    {value: 'area-13', viewValue: 'Kollupitiya'}, {value: 'area-14', viewValue: 'Kottawa'}, {value: 'area-15', viewValue: 'Maharagama'},
-    {value: 'area-16', viewValue: 'Meegoda'}, {value: 'area-17', viewValue: 'Mirihana'}, {value: 'area-18', viewValue: 'Moragahahena'},
-    {value: 'area-19', viewValue: 'Moratumulla'}, {value: 'area-20', viewValue: 'Moratuwa'}, {value: 'area-21', viewValue: 'Mt-Lavinia'},
-    {value: 'area-22', viewValue: 'Padukka'}, {value: 'area-23', viewValue: 'Panadura'}, {value: 'area-24', viewValue: 'Piliyandala'},
-    {value: 'area-25', viewValue: 'Welikada'}, {value: 'area-26', viewValue: 'Wellawatte'}
+    {value: 'area-01', viewValue: 'Agulana' /*moratuva*/},
+    {value: 'area-02', viewValue: 'Athurugiriya' /* homagama*/},
+    {value: 'area-03', viewValue: 'Bambalapitiya' },
+    {value: 'area-04', viewValue: 'Boralesgamuwa'},
+    {value: 'area-05', viewValue: 'Borella'},
+    { value: 'area-06', viewValue: 'Dehiwala' /* Dehiwala*/},
+    { value: 'area-07', viewValue: 'Homagama' /* homagama*/},
+    {value: 'area-08', viewValue: 'Hospital Police'},
+    {value: 'area-09', viewValue: 'Jayawardanapura'},
+    {value: 'area-10', viewValue: 'Kahathuduwa'},
+    {value: 'area-11', viewValue: 'Kirulapone'},
+    {value: 'area-12', viewValue: 'Kohuwala'},
+    {value: 'area-13', viewValue: 'Kollupitiya'},
+    { value: 'area-14', viewValue: 'Kottawa' /*Maharagama*/},
+    {value: 'area-15', viewValue: 'Maharagama' /*Maharagama*/},
+    {value: 'area-16', viewValue: 'Meegoda'},
+    {value: 'area-17', viewValue: 'Mirihana'},
+    {value: 'area-18', viewValue: 'Moragahahena'},
+    {value: 'area-19', viewValue: 'Moratumulla'},
+    {value: 'area-20', viewValue: 'Moratuwa'},
+    { value: 'area-21', viewValue: 'Mt-Lavinia' /* Ratmalana */},
+    {value: 'area-22', viewValue: 'Padukka'},
+    { value: 'area-23', viewValue: 'Panadura' /*Panadura */},
+    {value: 'area-24', viewValue: 'Piliyandala' /*kasbawa*/ },
+    { value: 'area-25', viewValue: 'Welikada' /* Sri Jayawardanapura Kotte */},
+    {value: 'area-26', viewValue: 'Wellawatte'}
   ];
 
 
@@ -209,59 +225,59 @@ export class AddComponent implements OnInit {
     this.form.gaspecimens = this.selectedOptions1;    
     this.form.mrispecimens = this.selectedOptions2;    
     this.form.otherspecimens = this.selectedOptions3;
-    if(this.form.policescenephoto=="no"){
-      this.form.policefoldername="None"
+    if(this.form.policescenephoto=='no'){
+      this.form.policefoldername='None'
     }
     this.adddeceaseddetails();
   }
   reset(){
     this.submitted=false;
-    this.errors["srjdup"]=false;
-    this.errors["overall"]=false;    
-    this.errors["gact"]=false;
-    this.errors["mriref"]=false;
-    this.errors["otherref"]=false;
+    this.errors['srjdup']=false;
+    this.errors['overall']=false;    
+    this.errors['gact']=false;
+    this.errors['mriref']=false;
+    this.errors['otherref']=false;
   }
   adddeceaseddetails(){
     this.adddeceased.adddeceased(this.form).subscribe(
       data => { 
-        if(data["message"]="success"){
-          this.errors["srjdup"]=false;
-          this.errors["overall"]=false;
-          this.errors["gact"]=false;
-          this.errors["mriref"]=false;
-          this.errors["otherref"]=false;
+        if(data['message']='success'){
+          this.errors['srjdup']=false;
+          this.errors['overall']=false;
+          this.errors['gact']=false;
+          this.errors['mriref']=false;
+          this.errors['otherref']=false;
 
 
         }               
       },
       error => 
       {             
-        if(error.error.error=="SRJ no already exists") {   
-          this.errors["srjdup"]=true;
+        if(error.error.error=='SRJ no already exists') {   
+          this.errors['srjdup']=true;
         }
         else
-          this.errors["srjdup"]=false;
-        if(error.error.error=="ga ct") {   
-          this.errors["gact"]=true;
+          this.errors['srjdup']=false;
+        if(error.error.error=='ga ct') {   
+          this.errors['gact']=true;
         }
         else
-          this.errors["gact"]=false;
-        if(error.error.error=="mri ref") {   
-          this.errors["mriref"]=true;
+          this.errors['gact']=false;
+        if(error.error.error=='mri ref') {   
+          this.errors['mriref']=true;
         }
         else
-          this.errors["mriref"]=false;
-        if(error.error.error=="other ref") {   
-          this.errors["otherref"]=true;
+          this.errors['mriref']=false;
+        if(error.error.error=='other ref') {   
+          this.errors['otherref']=true;
         }
         else
-          this.errors["otherref"]=false;
-        if(error.error.error=="Oops something went wrong!"){
-          this.errors["overall"]=true;
+          this.errors['otherref']=false;
+        if(error.error.error=='Oops something went wrong!'){
+          this.errors['overall']=true;
         }
         else
-          this.errors["overall"]=false;
+          this.errors['overall']=false;
       }
     );
   }  
